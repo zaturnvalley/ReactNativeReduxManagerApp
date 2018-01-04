94f5cadafb187221278b13090119ea873ac11a51
zaturnvalley <zacharycmason@gmail.com>
2018-01-03 19:57:58 -0800

    added firebase to npm, pulled in config into comp

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount(){
+    const config = {
+    apiKey: 'AIzaSyB7cAb2AbWAJDmCKWshYU8nDZ27tLvXrmc',
+    authDomain: 'manager-aaba3.firebaseapp.com',
+    databaseURL: 'https://manager-aaba3.firebaseio.com',
+    projectId: 'manager-aaba3',
+    storageBucket: 'manager-aaba3.appspot.com',
+    messagingSenderId: '385490799188'
+    };
+
+    firebase.initializeApp(config);
+  }
+  
   render(){
     return (
       <Provider store={createStore(reducers)}>
