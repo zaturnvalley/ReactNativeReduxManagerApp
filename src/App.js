69634b71fe464e09e4ecc2e93c634abf75ea8a64
zaturnvalley <zacharycmason@gmail.com>
2018-01-03 20:19:07 -0800

    added login form component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount(){
@@ -18,15 +19,11 @@ class App extends Component {
 
     firebase.initializeApp(config);
   }
-  
+
   render(){
     return (
       <Provider store={createStore(reducers)}>
-        <View>
-          <Text>
-            Hello
-          </Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   }
