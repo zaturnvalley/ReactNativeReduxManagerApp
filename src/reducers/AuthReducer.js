94453aea2bf2eb415c2bfdddd7e31e6348a04481
zaturnvalley <zacharycmason@gmail.com>
2018-01-08 15:47:15 -0800

    initial state default;

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -3,7 +3,10 @@ import {
   PASSWORD_CHANGED 
 } from '../actions/types';
 
-const INITIAL_STATE = { email: '' };
+const INITIAL_STATE = { 
+  email: '',
+  password: '' 
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
