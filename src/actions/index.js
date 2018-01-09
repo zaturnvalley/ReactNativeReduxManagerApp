586d7e8216210141d3e473c4be552ddcaea13743
zaturnvalley <zacharycmason@gmail.com>
2018-01-09 14:19:45 -0800

    created loginUserSuccess method

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,14 +22,17 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => {
-        dispatch({ type: LOGIN_USER_SUCCESS, payload: user })
-      })
+      .then(user => loginUserSuccess(dispatch, user))
       .catch(() => {
         firebase.auth().createUserWithEmailAndPasword(email, password)
-          .then(user => {
-            dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
-          });
+          .then(user => loginUserSuccess(dispatch, user));
       });
   };
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
 };
