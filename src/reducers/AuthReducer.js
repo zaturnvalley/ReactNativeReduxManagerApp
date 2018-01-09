d4152d6b9ec74aa024cd0150868ceaf3355ae0c1
zaturnvalley <zacharycmason@gmail.com>
2018-01-09 15:32:28 -0800

    updated authreduc w LOGIN_USER_FAIL

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,13 +1,15 @@
 import { 
   EMAIL_CHANGED, 
   PASSWORD_CHANGED,
-  LOGIN_USER_SUCCESS 
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL 
 } from '../actions/types';
 
 const INITIAL_STATE = { 
   email: '',
   password: '',
-  user: '' 
+  user: null,
+  error: '' 
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -18,6 +20,8 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, password: action.payload };
     case LOGIN_USER_SUCCESS:
       return { ...state, user: action.payload };  
+    case LOGIN_USER_FAIL:
+      return { ...state, error: 'Authentication Failed.'}
     default:
       return state;
   }
