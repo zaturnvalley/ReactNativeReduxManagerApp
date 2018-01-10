ce8aec633ef58ce7773a3ddb250be21b7cb1d800
zaturnvalley <zacharycmason@gmail.com>
2018-01-10 12:04:19 -0800

    LOGIN_USER case and loading bool

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -2,14 +2,16 @@ import {
   EMAIL_CHANGED, 
   PASSWORD_CHANGED,
   LOGIN_USER_SUCCESS,
-  LOGIN_USER_FAIL 
+  LOGIN_USER_FAIL,
+  LOGIN_USER 
 } from '../actions/types';
 
 const INITIAL_STATE = { 
   email: '',
   password: '',
   user: null,
-  error: '' 
+  error: '',
+  loading: false 
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -18,6 +20,8 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
+    case LOGIN_USER:
+      return { ...state, loading: true };
     case LOGIN_USER_SUCCESS:
       return { ...state, user: action.payload, error: '' };  
     case LOGIN_USER_FAIL:
