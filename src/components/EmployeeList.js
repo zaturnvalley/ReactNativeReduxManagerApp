89bba21bb2676dddff4ef5250eee960aa409785c
zaturnvalley <zacharycmason@gmail.com>
2018-01-18 13:23:56 -0800

    added employeelist

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render(){
+    return (
+      <View>
+      <Text>Stuff</Text>
+      <Text>Stuff</Text>
+      <Text>Stuff</Text>
+      </View>
+    );
+  }
+}
+export default EmployeeList;
