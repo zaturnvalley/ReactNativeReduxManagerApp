9372d377d04c469f6d269b6a7d5e1bb6682ac99b
zaturnvalley <zacharycmason@gmail.com>
2018-01-25 12:42:55 -0800

    push in name phone shift into db

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -14,4 +14,5 @@ export const employeeCreate = ({ name, phone, shift}) => {
   const { currentUser } = firebase.auth();
 
   firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift });
 };
