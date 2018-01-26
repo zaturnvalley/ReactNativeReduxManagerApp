9cdd086df8543efc215c72cf6122970a5530e101
zaturnvalley <zacharycmason@gmail.com>
2018-01-25 17:07:18 -0800

    added new type EMPLOYEES_FETCH_SUCCESS

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
