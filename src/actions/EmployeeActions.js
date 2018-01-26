4ccf53779c63e33665ae8ac4ed84cf0d5c3c45e3
zaturnvalley <zacharycmason@gmail.com>
2018-01-25 16:59:59 -0800

    created employeesFetch action

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -24,3 +24,11 @@ export const employeeCreate = ({ name, phone, shift}) => {
       });    
     };
 };
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+
+  };
+};
