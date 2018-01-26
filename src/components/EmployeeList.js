b68affa5ebae9a69dd04d8ee4c7e2968baf11a47
zaturnvalley <zacharycmason@gmail.com>
2018-01-26 13:49:11 -0800

    imported employeesFetch and connect to employeeslist

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
+import { employeesFetch } from '../actions'
 
 class EmployeeList extends Component {
   render(){
     return (
       <View>
-      <Text>Stuff</Text>
-      <Text>Stuff</Text>
-      <Text>Stuff</Text>
+        <Text>Stuff</Text>
+        <Text>Stuff</Text>
+        <Text>Stuff</Text>
       </View>
     );
   }
 }
-export default EmployeeList;
+export default connect(null, { employeesFetch })(EmployeeList);
