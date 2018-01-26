53d22ccd63159d67f9f8c7c215076fc3f782ab3e
zaturnvalley <zacharycmason@gmail.com>
2018-01-25 17:04:13 -0800

    on call back with snapshot method

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -27,8 +27,11 @@ export const employeeCreate = ({ name, phone, shift}) => {
 
 export const employeesFetch = () => {
   const { currentUser } = firebase.auth();
+
   return (dispatch) => {
     firebase.database().ref(`/users/${currentUser.uid}/employees`)
-
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+      });
   };
 };
