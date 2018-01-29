a494dbc5a64160f0c7bf5d9103b43e2f989b8e5e
zaturnvalley <zacharycmason@gmail.com>
2018-01-29 14:05:26 -0800

    created listview w datasource

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { employeesFetch } from '../actions'
 
 class EmployeeList extends Component {
   componentWillMount(){
     this.props.employeesFetch();
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !=== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.employees);
   }
-  
+
   render(){
     return (
       <View>
