36a7a3cb633bf2af798b27efdb19697bb84ee551
zaturnvalley <zacharycmason@gmail.com>
2018-02-06 16:42:51 -0800

    omitted hardcoded text for prop, onaccept decline methods added

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -3,17 +3,25 @@ import { Text, View, Modal } from 'react-native';
 import { CardSection } from './CardSection';
 import { Button } from './Button';
 
-const Confirm = () => {
+const Confirm = ({ children }) => {
+  onAccept(){
+
+  }
+
+  onDecline(){
+
+  }
+  
   return (
       <Modal>
         <View>
           <CardSection>
-            <Text>Do you want to fire this employee?</Text>
+            <Text>{children}</Text>
           </CardSection>
 
           <CardSection>
-            <Button>Yes</Button>
-            <Button>No</Button>
+            <Button onPress={onAccept}>Yes</Button>
+            <Button onPress={onDecline}>No</Button>
           </CardSection>
         </View>
       </Modal>
