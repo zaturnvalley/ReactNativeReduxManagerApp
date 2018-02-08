1011343f4ac0ebe61e070cc5aba1edb4d6ca4303
zaturnvalley <zacharycmason@gmail.com>
2018-02-08 14:51:05 -0800

    employeeDelete pops back to previous scene

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -58,7 +58,7 @@ export const employeeDelete = ({ uid }) => {
     firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
       .remove()
       .then(() => {
-        Actions.employeeList({ type: 'reset' });
+        Actions.pop();
       });
   };
 };
--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
@@ -27,7 +27,9 @@ class EmployeeEdit extends Component {
     Communications.text(phone, `Your upcoming shift is on ${shift}`);
   }
   onAccept() {
-    
+    const { uid } = this.props.employee;
+
+    this.props.employeeDelete({ uid });
   }
 
   onDecline() {
@@ -76,5 +78,5 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps, { 
-  employeeUpdate, employeeSave 
+  employeeUpdate, employeeSave, employeeDelete 
 })(EmployeeEdit);
